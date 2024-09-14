d2cc6329866fa56a0c6dd91f7c6c586b6e9d5c00
zekiblue <hi.zekiblue@gmail.com>
2024-09-14 10:03:08 +0300

    :recycle: Reorder

--- src/CCLOHook.sol
+++ src/CCLOHook.sol
@@ -25,7 +25,6 @@ import {Client} from "chainlink-local/lib/ccip/contracts/src/v0.8/ccip/libraries
 import {IRouterClient} from "chainlink-local/lib/ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {IRouter} from "chainlink-local/lib/ccip/contracts/src/v0.8/ccip/interfaces/IRouter.sol";
 
-
 contract CCLOHook is CCIPReceiver, BaseHook {
     using CurrencyLibrary for Currency;
     using CurrencySettler for Currency;
@@ -35,58 +34,18 @@ contract CCLOHook is CCIPReceiver, BaseHook {
     using StateLibrary for IPoolManager;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    // CCIP
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
     // Constants
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
     bytes internal constant ZERO_BYTES = bytes("");
 
-    // Custom errors to provide more descriptive revert messages.
-    error NoMessageReceived(); // Used when trying to access a message but no messages have been received.
-    error IndexOutOfBound(uint256 providedIndex, uint256 maxIndex); // Used when the provided index is out of bounds.
-    error MessageIdNotExist(bytes32 messageId); // Used when the provided message ID does not exist.
-    error NothingToWithdraw(); // Used when trying to withdraw Ether but there's nothing to withdraw.
-    error FailedToWithdrawEth(address owner, address target, uint256 value); // Used when the withdrawal of Ether fails.
-    error InsufficientFeeTokenAmount(); // Used when the contract balance isn't enough to pay fees.
-
-    // Event emitted when a message is sent to another chain.
-    event MessageSent(
-        bytes32 indexed messageId, // The unique ID of the message.
-        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
-        address receiver, // The address of the receiver on the destination chain.
-        string message, // The message being sent.
-        Client.EVMTokenAmount tokenAmount, // The token amount that was sent.
-        uint256 fees // The fees paid for sending the message.
-    );
-
-    // Event emitted when a message is received from another chain.
-    event MessageReceived(
-        bytes32 indexed messageId, // The unique ID of the message.
-        uint64 indexed sourceChainSelector, // The chain selector of the source chain.
-        address sender, // The address of the sender from the source chain.
-        string message, // The message that was received.
-        Client.EVMTokenAmount tokenAmount // The token amount that was received.
-    );
-
-    // Struct to hold details of a message.
-    struct Message {
-        uint64 sourceChainSelector; // The chain selector of the source chain.
-        address sender; // The address of the sender.
-        string message; // The content of the message.
-        address token; // received token.
-        uint256 amount; // received amount.
-    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Variables
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Storage variables.
     bytes32[] public receivedMessages; // Array to keep track of the IDs of received messages.
     mapping(bytes32 => Message) public messageDetail; // Mapping from message ID to Message struct, storing details of each received message.
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    // State variables
-    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Authorized user address
     address public authorizedUser;
@@ -97,46 +56,40 @@ contract CCLOHook is CCIPReceiver, BaseHook {
     // Mapping of strategy IDs to their respective liquidity distributions
     mapping(PoolId => mapping(uint256 => Strategy)) internal strategies;
 
-    event Log(string message);
-    event Log2(uint256 message);
-    event Log3(int128 message);
-
-    //    // Mapping of pool IDs to their respective cross-chain orders
-    //    mapping(PoolId => CrossChainOrder) public ordersToBeFilled;
-
-    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
-        return Hooks.Permissions({
-            beforeInitialize: false,
-            afterInitialize: false,
-            beforeAddLiquidity: true,
-            afterAddLiquidity: false,
-            beforeRemoveLiquidity: false,
-            afterRemoveLiquidity: false,
-            beforeSwap: false,
-            afterSwap: false,
-            beforeDonate: false,
-            afterDonate: false,
-            beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: false,
-            afterAddLiquidityReturnDelta: false,
-            afterRemoveLiquidityReturnDelta: false
-        });
-    }
-
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    // Structs
+    // Events
     ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // Event emitted when a cross-chain order is created
-    event CrossChainOrderCreated(
-        PoolId poolId, uint256 token0Amount, uint256 token1Amount, int24 lowerTick, int24 upperTick
-    );
-
-    //    // Event emitted when a cross-chain order is fulfilled
-    //    event CrossChainOrderFulfilled(PoolId poolId);
 
     // Event emitted when a new strategy is added
     event StrategyAdded(PoolId poolId, uint256 strategyId, uint256[] chainIds, uint256[] liquidityPercentages);
+
+    // Event emitted when a message is sent to another chain.
+    event MessageSent( // The unique ID of the message.
+        // The chain selector of the destination chain.
+        // The address of the receiver on the destination chain.
+        // The message being sent.
+        // The token amount that was sent.
+        // The fees paid for sending the message.
+        bytes32 indexed messageId,
+        uint64 indexed destinationChainSelector,
+        address receiver,
+        string message,
+        Client.EVMTokenAmount tokenAmount,
+        uint256 fees
+    );
+
+    // Event emitted when a message is received from another chain.
+    event MessageReceived( // The unique ID of the message.
+        // The chain selector of the source chain.
+        // The address of the sender from the source chain.
+        // The message that was received.
+        // The token amount that was received.
+        bytes32 indexed messageId,
+        uint64 indexed sourceChainSelector,
+        address sender,
+        string message,
+        Client.EVMTokenAmount tokenAmount
+    );
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Structs
@@ -159,18 +112,37 @@ contract CCLOHook is CCIPReceiver, BaseHook {
         uint256[] percentages;
     }
 
-    //    // Struct representing a cross-chain order
-    //    struct CrossChainOrder {
-    //        PoolId poolId;
-    //        uint256 token0Amount;
-    //        uint256 token1Amount;
-    //        int24 lowerTick;
-    //        int24 upperTick;
-    //    }
+    // Struct to hold details of a message.
+    struct Message {
+        uint64 sourceChainSelector; // The chain selector of the source chain.
+        address sender; // The address of the sender.
+        string message; // The content of the message.
+        address token; // received token.
+        uint256 amount; // received amount.
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Errors
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Custom errors to provide more descriptive revert messages.
+    error NoMessageReceived(); // Used when trying to access a message but no messages have been received.
+    error IndexOutOfBound(uint256 providedIndex, uint256 maxIndex); // Used when the provided index is out of bounds.
+    error MessageIdNotExist(bytes32 messageId); // Used when the provided message ID does not exist.
+    error NothingToWithdraw(); // Used when trying to withdraw Ether but there's nothing to withdraw.
+    error FailedToWithdrawEth(address owner, address target, uint256 value); // Used when the withdrawal of Ether fails.
+    error InsufficientFeeTokenAmount(); // Used when the contract balance isn't enough to pay fees.
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Constructor
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     /// @notice Constructor initializes the contract with the router address.
     /// @param router The address of the router contract.
-    constructor(IPoolManager _poolManager, address _authorizedUser, uint256 _hookChainId, address router) BaseHook(_poolManager) CCIPReceiver(router) {
+    constructor(IPoolManager _poolManager, address _authorizedUser, uint256 _hookChainId, address router)
+        BaseHook(_poolManager)
+        CCIPReceiver(router)
+    {
         hookChainId = _hookChainId;
         authorizedUser = _authorizedUser;
     }
@@ -183,6 +155,29 @@ contract CCLOHook is CCIPReceiver, BaseHook {
     modifier onlyAuthorized() {
         require(msg.sender == authorizedUser, "Only authorized users can add strategies");
         _;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Hook definitions
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: true,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: false,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -328,12 +323,8 @@ contract CCLOHook is CCIPReceiver, BaseHook {
         uint256 amount
     ) external returns (bytes32 messageId) {
         // set the tokent amounts
-        Client.EVMTokenAmount[]
-            memory tokenAmounts = new Client.EVMTokenAmount[](1);
-        Client.EVMTokenAmount memory tokenAmount = Client.EVMTokenAmount({
-            token: token,
-            amount: amount
-        });
+        Client.EVMTokenAmount[] memory tokenAmounts = new Client.EVMTokenAmount[](1);
+        Client.EVMTokenAmount memory tokenAmount = Client.EVMTokenAmount({token: token, amount: amount});
         tokenAmounts[0] = tokenAmount;
         // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
         Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
@@ -359,63 +350,34 @@ contract CCLOHook is CCIPReceiver, BaseHook {
         if (address(this).balance < fees) revert InsufficientFeeTokenAmount();
 
         // Send the message through the router and store the returned message ID
-        messageId = router.ccipSend{value: fees}(
-            destinationChainSelector,
-            evm2AnyMessage
-        );
+        messageId = router.ccipSend{value: fees}(destinationChainSelector, evm2AnyMessage);
 
         // Emit an event with message details
-        emit MessageSent(
-            messageId,
-            destinationChainSelector,
-            receiver,
-            message,
-            tokenAmount,
-            fees
-        );
+        emit MessageSent(messageId, destinationChainSelector, receiver, message, tokenAmount, fees);
 
         // Return the message ID
         return messageId;
     }
 
     /// handle a received message
-    function _ccipReceive(
-        Client.Any2EVMMessage memory any2EvmMessage
-    ) internal override {
+    function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal override {
         bytes32 messageId = any2EvmMessage.messageId; // fetch the messageId
         uint64 sourceChainSelector = any2EvmMessage.sourceChainSelector; // fetch the source chain identifier (aka selector)
         address sender = abi.decode(any2EvmMessage.sender, (address)); // abi-decoding of the sender address
-        Client.EVMTokenAmount[] memory tokenAmounts = any2EvmMessage
-            .destTokenAmounts;
+        Client.EVMTokenAmount[] memory tokenAmounts = any2EvmMessage.destTokenAmounts;
         address token = tokenAmounts[0].token; // we expect one token to be transfered at once but of course, you can transfer several tokens.
         uint256 amount = tokenAmounts[0].amount; // we expect one token to be transfered at once but of course, you can transfer several tokens.
         string memory message = abi.decode(any2EvmMessage.data, (string)); // abi-decoding of the sent string message
         receivedMessages.push(messageId);
-        Message memory detail = Message(
-            sourceChainSelector,
-            sender,
-            message,
-            token,
-            amount
-        );
+        Message memory detail = Message(sourceChainSelector, sender, message, token, amount);
         messageDetail[messageId] = detail;
 
-        emit MessageReceived(
-            messageId,
-            sourceChainSelector,
-            sender,
-            message,
-            tokenAmounts[0]
-        );
+        emit MessageReceived(messageId, sourceChainSelector, sender, message, tokenAmounts[0]);
     }
 
     /// @notice Get the total number of received messages.
     /// @return number The total number of received messages.
-    function getNumberOfReceivedMessages()
-        external
-        view
-        returns (uint256 number)
-    {
+    function getNumberOfReceivedMessages() external view returns (uint256 number) {
         return receivedMessages.length;
     }
 
@@ -427,28 +389,14 @@ contract CCLOHook is CCIPReceiver, BaseHook {
     /// @return message The received message.
     /// @return token The received token.
     /// @return amount The received token amount.
-    function getReceivedMessageDetails(
-        bytes32 messageId
-    )
+    function getReceivedMessageDetails(bytes32 messageId)
         external
         view
-        returns (
-            uint64 sourceChainSelector,
-            address sender,
-            string memory message,
-            address token,
-            uint256 amount
-        )
+        returns (uint64 sourceChainSelector, address sender, string memory message, address token, uint256 amount)
     {
         Message memory detail = messageDetail[messageId];
         if (detail.sender == address(0)) revert MessageIdNotExist(messageId);
-        return (
-            detail.sourceChainSelector,
-            detail.sender,
-            detail.message,
-            detail.token,
-            detail.amount
-        );
+        return (detail.sourceChainSelector, detail.sender, detail.message, detail.token, detail.amount);
     }
 
     /// @notice Fetches details of a received message by its position in the received messages list.
@@ -460,9 +408,7 @@ contract CCLOHook is CCIPReceiver, BaseHook {
     /// @return message The received message.
     /// @return token The received token.
     /// @return amount The received token amount.
-    function getReceivedMessageAt(
-        uint256 index
-    )
+    function getReceivedMessageAt(uint256 index)
         external
         view
         returns (
@@ -474,18 +420,12 @@ contract CCLOHook is CCIPReceiver, BaseHook {
             uint256 amount
         )
     {
-        if (index >= receivedMessages.length)
+        if (index >= receivedMessages.length) {
             revert IndexOutOfBound(index, receivedMessages.length - 1);
+        }
         messageId = receivedMessages[index];
         Message memory detail = messageDetail[messageId];
-        return (
-            messageId,
-            detail.sourceChainSelector,
-            detail.sender,
-            detail.message,
-            detail.token,
-            detail.amount
-        );
+        return (messageId, detail.sourceChainSelector, detail.sender, detail.message, detail.token, detail.amount);
     }
 
     /// @notice Fetches the details of the last received message.
@@ -517,14 +457,7 @@ contract CCLOHook is CCIPReceiver, BaseHook {
         // Fetch the details of the last received message
         Message memory detail = messageDetail[messageId];
 
-        return (
-            messageId,
-            detail.sourceChainSelector,
-            detail.sender,
-            detail.message,
-            detail.token,
-            detail.amount
-        );
+        return (messageId, detail.sourceChainSelector, detail.sender, detail.message, detail.token, detail.amount);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
